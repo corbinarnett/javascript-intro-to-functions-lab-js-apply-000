cdb79d3709a1a4d52188cb7462dde6fc55087f10
creative-debugger-6293 <corbin.c.arnett@gmail.com>
2019-06-02 07:03:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-   string.toLowerCase() === lowercase;
-   return "I can't hear you!";
+   var lowercase = "I can't hear you!"
+   lowercase.toLowerCase() === lowercase;
 }
